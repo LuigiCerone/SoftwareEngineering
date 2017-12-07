0b69bef9580ad96561604982ae43d239c39aaf7b
luigi.cerone <luigiceroneaq@gmail.com>
2017-12-07 19:33:58 +0100

    MySQL db dump added

--- src/Controller/ControllerSignals.java
+++ src/Controller/ControllerSignals.java
@@ -21,11 +21,10 @@ public class ControllerSignals implements Runnable {
         // TODO Write the following in a file.
         // new ReadDataDAO().insert(this.readData);
 
-        // TODO Check if the cluster exists otherwise create it.
         Cluster cluster = new ClusterDAO().findClusterByIdOrInsert(readData);
-        // TODO Check if the robot exists otherwise create it.
+
         Robot robot = new RobotDAO().findRobotByIdOrInsert(readData);
-//        mongoDB.insertReadingsData(readData);
-//        mongoDB.insertRobotInCluster(readData);
+
+
     }
 }
--- src/db.sql
+++ src/db.sql
@@ -1,10 +1,59 @@
-CREATE TABLE SEProject.read_data
+CREATE DATABASE SEProject;
+USE SEProject;
+
+CREATE TABLE cluster
 (
-    id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    robotId varchar(20) NOT NULL,
-    clusterId varchar(20) NOT NULL,
-    zoneId varchar(20) NOT NULL,
-    signals int(1) NOT NULL,
-    value int(6) NOT NULL,
-    timestamp datetime NOT NULL
+  id     VARCHAR(30)        NOT NULL
+    PRIMARY KEY,
+  zoneId VARCHAR(30)        NOT NULL,
+  ir     DOUBLE DEFAULT '0' NOT NULL
 );
+
+CREATE TABLE read_data
+(
+  id        INT AUTO_INCREMENT
+    PRIMARY KEY,
+  robotId   VARCHAR(20) NOT NULL,
+  clusterId VARCHAR(20) NOT NULL,
+  zoneId    VARCHAR(20) NOT NULL,
+  signals   INT(1)      NOT NULL,
+  value     INT(6)      NOT NULL,
+  timestamp VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE robot
+(
+  id        VARCHAR(30)        NOT NULL
+    PRIMARY KEY,
+  clusterId VARCHAR(30)        NOT NULL,
+  ir        DOUBLE DEFAULT '0' NOT NULL,
+  count     INT DEFAULT '0'    NOT NULL,
+  downTime  INT DEFAULT '0'    NULL
+);
+
+CREATE TRIGGER signalsinit
+AFTER INSERT ON robot
+FOR EACH ROW
+  BEGIN
+    DECLARE now VARCHAR(40);
+    SET now = NOW();
+    INSERT INTO signals (id, number, value, timestamp, robotId) VALUES (NULL, 1, 1, now, NEW.id);
+    INSERT INTO signals (id, number, value, timestamp, robotId) VALUES (NULL, 2, 1, now, NEW.id);
+    INSERT INTO signals (id, number, value, timestamp, robotId) VALUES (NULL, 3, 1, now, NEW.id);
+    INSERT INTO signals (id, number, value, timestamp, robotId) VALUES (NULL, 4, 1, now, NEW.id);
+    INSERT INTO signals (id, number, value, timestamp, robotId) VALUES (NULL, 5, 1, now, NEW.id);
+    INSERT INTO signals (id, number, value, timestamp, robotId) VALUES (NULL, 6, 1, now, NEW.id);
+    INSERT INTO signals (id, number, value, timestamp, robotId) VALUES (NULL, 7, 1, now, NEW.id);
+  END;
+
+CREATE TABLE signals
+(
+  id        INT AUTO_INCREMENT
+    PRIMARY KEY,
+  number    INT                    NOT NULL,
+  value     TINYINT(1) DEFAULT '1' NOT NULL,
+  timestamp VARCHAR(40)            NOT NULL,
+  robotId   VARCHAR(30)            NOT NULL
+);
+
+L
